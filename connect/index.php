f99db715440da840fc2a9e1364291345d21795d3
heyeon <zainabg@gmail.com>
2012-10-27 20:23:26 -0500

    first set of ui changes

--- connect/index.php
+++ connect/index.php
@@ -2,9 +2,11 @@
 
 <head>
 <title> Connect </title>
+<!-- Bootstrap -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 
-<body>
+<body margin-left="10px">
 <?php
 include("library.php");
 session_start();
@@ -14,57 +16,54 @@ session_start();
 if(!sessionCheck()){
 
 ?>
-<div>
-
-</div>
-</br>
-
-<div>
-<form name="signup" action="signup.php" method="post">
-<table>
-<tr><td>
-<label> First Name: </label>
+<h1> Welcome to Connect : Madison's Distribtued Chat System </h1>
+<h3>Login</h3>
+<form class="form-inline" action="login.php" method = "POST">
+<input type ="text" class="input-small" placeholder="Username" name="handle"> </input> 
+<input type = "password" class="input-small" placeholder="Pasword" name="password"> </input> 
+<input class="btn" type="submit" name="login" value="Log In"> </input>
+</form>
+<h3>Register now</h3>
+<form class="form-horizontal" name="signup" action="signup.php" method="post">
+<div class="control-group">
+<label class="control-label"><strong> First Name: </strong></label>
+<div class ="controls">
 <input type="text" name="fname"/>
-</td></tr>
-<tr><td>
-<label> Last Name: </label>
+</div>
+</div>
+<div class="control-group">
+<label class="control-label"><strong> Last Name: </strong></label>
+<div class="controls">
 <input type="text" name="lname"/>
-</td> </tr>
-<tr><td>
-<label> Connect Handle: 
-</label>
+</div></div>
+<div class="control-group">
+<label class="control-label"> <strong> Connect Handle: </strong></label>
+<div class="controls">
 <input type="text" name="handle"/>
-</td></tr>
-<tr><td>
-<label> Connect Password: </label>
+</div></div>
+<div class="control-group">
+<label class="control-label"><strong> Connect Password: </strong></label>
+<div class="controls">
 <input type="password" name="passwd"/>
-</td></tr>
-<tr><td>
-<label> E-mail: </label>
-<input type="text" name="email"/>
-</td></tr>
-<tr><td>
-<input type="submit" value="Sign Up!!"> </input>
-</td></tr>
-</table>
+</div></div>
+<div class="control-group">
+<label class="control-label"><strong> E-mail: </strong> </label>
+<div class="controls">
+<input clas="btn" type="text" name="email"/>
+</div></div>
+<div class="control-group">
+<div class="controls">
+<input class="btn btn-large btn-primary" type="submit" value="Sign Up!!">
+</div></div>
 </form>
 </div>
 <div>
 <table border="1">
 <tr>
-<form action="login.php" method = "POST">
-<label>Username: </label>
-<input type ="text" name="handle"> </input> 
-</br>
-</br>
-</br>
-<label>Password: </label>
-<input type = "password" name="password"> </input> </br>
-</br>
-<input type="submit" name="login" value="Log In"> </input>
-</form>
 </table>
 </div>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="js/bootstrap.min.js"></script>
 </body>
 <?php
 }
