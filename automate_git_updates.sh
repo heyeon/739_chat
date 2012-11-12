88e7851a0c3471659b114e68b6a407b99334624b
heyeon <zainabg@gmail.com>
2012-11-12 20:55:31 +0000

    Changing cron frequency to twice a day. Testing change on l1. Will propogate across all instances tonight

--- automate_git_updates.sh
+++ automate_git_updates.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-cd /home/ubuntu/739_mirror
-echo "Entering mirror directory"
+date
 git clone https://github.com/heyeon/739_chat
 echo "Cloned remote into mirror"
-sudo cp * /home/ubuntu/739
+sudo cp -rf 739_chat /home/ubuntu/739
 echo "Copying files from mirror to master"
-sudo rm -r /home/ubuntu/739_mirror
+sudo rm -rf /home/ubuntu/739_chat
 echo "Removing files from mirror"
