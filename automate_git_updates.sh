3876546fa51f0dcc7827bbb6df8e587c3a428c55
heyeon <zainabg@gmail.com>
2012-11-22 22:07:06 +0000

    Fix to handing copying files to server

--- automate_git_updates.sh
+++ automate_git_updates.sh
@@ -8,7 +8,7 @@ sudo git clone https://github.com/heyeon/739_chat
 echo "Cloned remote into mirror"
 sudo cp -rf 739_chat /home/ubuntu/739
 echo "Copying files from mirror to master"
-sudo cp -rf 739_chat /opt/lampp/htdocs/
+sudo cp -rf 739_chat/* /opt/lampp/htdocs/
 echo "Copying files from mirror to server"
 sudo rm -rf /home/ubuntu/739_chat
 echo "Removing files from mirror"
