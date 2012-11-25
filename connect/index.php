46a3c7a541246da89f60cd8fc23471518b9db1b6
heyeon <zainabg@gmail.com>
2012-11-25 06:58:31 +0000

    working on fb connect again

--- connect/index.php
+++ connect/index.php
@@ -21,6 +21,7 @@
 		<script src="http://connect.facebook.net/en_US/all.js"></script>
 		<script src="js/jquery-1.8.2.js"> </script>
 		<script src="js/jquery.validate.js"> </script>
+		<script src="chat_post_msg.js"></script>
 		<script type="text/javascript">  
 			FB.init
 			({
@@ -28,111 +29,13 @@
 				status:true, xfbml:true
 			});
 			
-			var handle;
-			
-			function scrollBox()
-			{
-				$("chatTexts").scrollTop(200); // this was an attempt to make the chatText textarea autoscroll but something is missing. May be you can have a look.
-			}
-			
-			function submitChat()
-			{
-				if ($('#inputTextArea').val() != "")
-				{
-					$.post("ajax.php", {newMessage:$('#inputTextArea').val(),sender:$("#handle").html()}, 
-						function(data)
-						{
-							$("#inputTextArea").val("");
-							scrollBox();
-						}
-					);
-				}
-			}
-			
-			
-			
-			function update()
-			{
-				$.post("ajax.php", {chat:$('#inputTextArea').val(),sender:$("#handle").html()}, 
-				function(data)
-					{
-						if (data != "")
-						{
-							var messages = data.split("<br>");
-							var numNewMessages = messages.length;
-							for (i = 0; i < numNewMessages-1; i++)
-							{
-								$("#chatTexts").val($("#chatTexts").val()+ "\n"+messages[i]);
-							}
-						}
-						
-					}
-				); 
-				
-				$.post("ajax.php", {friendList:"friendList"}, 
-				function(data)
-					{
-						var frndList = data.split("<br>");
-						$('#friendList').empty();
-						var length = frndList.length;
-						for (i = 0; i < length-1; i++)
-						{
-							var row = $("<tr><td>" + frndList[i] +"</td><td> <img src = 'img/online.png' width='10px' height='10px' /></td></tr>");
-							$('#friendList').append(row);
-						}
-					}
-				); 
-				setTimeout('update()', 1000);
-			}
-			function checkUserName()
-			{	
-				if (!$("#handle").val())
-				{
-					$("#error2").text("");
-				}
-				else
-				{
-					$.post("ajax.php", {handle:$("#handle").val()}, 
-					function(data)
-						{
-							if (data == "false")
-							{
-								$("#error2").text("User name not available");
-							}
-						}		
-					);
-				}				
-			}
-			
-			$(document).ready
-			(
-				function()
-				{$("#error").text("");
-					update();
-					$("#loginForm").validate({ 
-					rules: { 
-						loginHandle: "required",
-						loginPassword: "required"
-					}}); 
-					$("#signup").validate({ 
-						rules: { 
-						fname: "required",
-						lname: "required",
-						email: {
-							required: true, 
-							email: true 
-					},
-					handle: "required",
-					passwd: "required"
-					}}); 
-	     
-				});
 		</script>
 	</head>
 	<body >
 		<?php
 			include("library.php");
-			session_start();
+			include_once("fbconnect.php");
+			//session_start();
 			if(!sessionCheck()){
 		?>
 		<div id="body" class="hero-unit">
@@ -161,8 +64,8 @@
 				<div class="control-group">
 					<div class="controls">
 						<input class="btn btn-large btn-primary" type="submit" value="Log In!!" ">
-						<h4> OR </h4>
-						<fb:login-button>Login with Facebook</fb:login-button>
+						<h4> TEST-OR </h4>
+						<a href="<?php echo $loginUrl;?>"><img src="images/f-connect.png" alt="Connect to your Facebook Account"></a>
 					</div>
 				</div>
 			</form>
