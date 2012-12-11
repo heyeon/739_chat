380850d789abcdf0a2c6b2ed44ade864b5720d92
heyeon <zainabg@gmail.com>
2012-12-11 16:05:01 -0600

    Revert chat post msg call changes

--- connect/index.php
+++ connect/index.php
@@ -21,7 +21,107 @@
 		<script src="http://connect.facebook.net/en_US/all.js"></script>
 		<script src="js/jquery-1.8.2.js"> </script>
 		<script src="js/jquery.validate.js"> </script>
-		<script src="chat_post_ajax.js"></script>
+		<script src="text/javascript">
+			var handle;
+			
+			function scrollBox()
+			{
+				$("chatTexts").scrollTop(200); 
+			}
+			
+			function submitChat()
+			{
+				if ($('#inputTextArea').val() != "")
+				{
+					$.post("ajax.php", {newMessage:$('#inputTextArea').val(),sender:$("#handle").html()}, 
+						function(data)
+						{
+							$("#inputTextArea").val("");
+							scrollBox();
+						}
+					);
+				}
+			}
+			
+			
+			
+			function update()
+			{
+				$.post("ajax.php", {chat:$('#inputTextArea').val(),sender:$("#handle").html()}, 
+				function(data)
+					{
+						if (data != "")
+						{
+							var messages = data.split("<br>");
+							var numNewMessages = messages.length;
+							for (i = 0; i < numNewMessages-1; i++)
+							{
+								$("#chatTexts").val($("#chatTexts").val()+ "\n"+messages[i]);
+							}
+						}
+						
+					}
+				); 
+				
+				$.post("ajax.php", {friendList:"friendList"}, 
+				function(data)
+					{
+						var frndList = data.split("<br>");
+						$('#friendList').empty();
+						var length = frndList.length;
+						for (i = 0; i < length-1; i++)
+						{
+							var row = $("<tr><td>" + frndList[i] +"</td><td> <img src = 'img/online.png' width='10px' height='10px' /></td></tr>");
+							$('#friendList').append(row);
+						}
+					}
+				); 
+				setTimeout('update()', 1000);
+			}
+			function checkUserName()
+			{	
+				if (!$("#handle").val())
+				{
+					$("#error2").text("");
+				}
+				else
+				{
+					$.post("ajax.php", {handle:$("#handle").val()}, 
+					function(data)
+						{
+							if (data == "false")
+							{
+								$("#error2").text("User name not available");
+							}
+						}		
+					);
+				}				
+			}
+			
+			$(document).ready
+			(
+				function()
+				{$("#error").text("");
+					update();
+					$("#loginForm").validate({ 
+					rules: { 
+						loginHandle: "required",
+						loginPassword: "required"
+					}}); 
+					$("#signup").validate({ 
+						rules: { 
+						fname: "required",
+						lname: "required",
+						email: {
+							required: true, 
+							email: true 
+					},
+					handle: "required",
+					passwd: "required"
+					}}); 
+	     
+				});
+    </script>
 		<script type="text/javascript">  
 			FB.init
 			({
